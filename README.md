741de635e96fb7b7e87572ec1e9a55bef6081161
ryo.nagaoka <ryo.nagaoka@leadingmark.jp>
2016-12-22 17:16:05 +0900

    Django Grappelli追加

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# sendgrid_example
-Django×SendGridのサンプルプロジェクト
+# Django SendGrid用サンプルプロジェクト
+https://github.com/charly24/django-sendgrid
+利用時のサンプルプロジェクト
+
+構築/動作の流れは以下参照。
+http://qiita.com/charly24/items/a820d1ff3a01be1cc475
+
+※管理画面を見やすくするために[Django Grappelli](http://grappelliproject.com/)を導入しています
--- sendgrid_sample/sendgrid_sample/settings.py
+++ sendgrid_sample/sendgrid_sample/settings.py
@@ -37,6 +37,7 @@ SENDGRID_EMAIL_PASSWORD = ''
 SENDGRID_API_KEY = ''
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
